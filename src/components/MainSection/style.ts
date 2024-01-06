a309cec59f86834329e9f046d7c15f153e32f151
Lucas Ribeiro Bonfílio de Lemos <lucasrblemos@gmail.com>
2024-01-06 17:20:48 -0300

    fix: modify title, text and image components

--- src/components/MainSection/style.ts
+++ src/components/MainSection/style.ts
@@ -4,25 +4,35 @@ export const Container = styled.section `
     display: flex;
     flex-direction: column;
     padding: 0 2rem;
+    justify-content: center;
+    align-items: center;
 `;
 
 export const SectionContent = styled.div `
     display: flex;
-    gap: 4rem;
+    align-items: center;
+    gap: 3rem;
 `;
 
 export const Title = styled.h1 `
-    font-size: 4rem;
-    width: 1rem; 
+    font-size: 5.5rem;
+    width: 300px; 
+    text-align: end;
+    font-weight: 500;
+    color: #C69043;
+    transform: translate(0, 10px);
+    line-height: 0.9;
 `;
 
 
 export const Text = styled.p `
-    font-size: .75rem;
+    font-size: .70rem;
+    align-self: end;
+    max-width: 250px;
 `;
 
 
 export const Image = styled.img `
-    width: 120px;
-    height: 120px;
+    width: 180px;
+    height: 180px;
 `;
